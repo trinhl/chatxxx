ec7973b62c5f30e70ee1f59c3ac7a9670e3921ab
truongphanuit <phantruong.bt@gmail.com>
2016-06-15 16:04:44 +0700

    xong khung chat, con thieu auto scroll bot

--- client/app/chat/chat.controller.js
+++ client/app/chat/chat.controller.js
@@ -220,7 +220,7 @@ startVideo() {
 }
 
 autoScrollBot() {
-  setInterval(function() {
+  setTimeout(function() {
     var elem = document.getElementById('message_list');
     elem.scrollTop = elem.scrollHeight + 200;
   }, 50);
